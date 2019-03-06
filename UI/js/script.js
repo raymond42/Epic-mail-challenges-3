78d3f7e9dc021a661de5d133fdeee83bf3fb61d4
Raymond Gakwaya <raymond42.gr@gmail.com>
2019-03-06 19:06:05 +0200

    [feature #164214816] add a forgot password page, now users can reset password before they are in their accounts

---
+++ UI/js/script.js
@@ -0,0 +1,6 @@
+const signupPage = document.getElementById("signup-page");
+const passwordReset = document.getElementById("reset");
+
+passwordReset.addEventListener('click', () => {
+    alert("A link with a reset password form has been sent your email");
+});
