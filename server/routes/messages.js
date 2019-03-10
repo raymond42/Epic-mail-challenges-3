5e9a68fe3c0eca1d2695ce95cef7a468fa7e04f8
Raymond Gakwaya <raymond42.gr@gmail.com>
2019-03-10 05:51:22 +0200

    [feature #164493477] add user api to fetch a specific email

--- server/routes/messages.js
+++ server/routes/messages.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import {composeMessages} from '../controllers/messages';
 import {getMessages} from '../controllers/messages';
+import {getOneMessage} from '../controllers/messages';
 const router = express.Router();
 
 
 router.post('/api/v1/messages', composeMessages);
 router.get('/api/v1/messages', getMessages);
-
+router.get('/api/v1/messages/:id', getOneMessage);
 
 
 
