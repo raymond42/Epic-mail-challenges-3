1cda72d857f681d489040650687e8af6c6e8ef7e
Raymond Gakwaya <raymond42.gr@gmail.com>
2019-03-12 12:37:27 +0200

    [feature #164493454] renaming post router of signup

--- server/routes/user.js
+++ server/routes/user.js
@@ -4,7 +4,7 @@ const router = express.Router();
 import {signup} from '../controllers/users';
 import {login} from '../controllers/users';
 
-router.post('/api/v1/users', signup);
+router.post('/api/v1/users/signup', signup);
 router.post('/api/v1/users/login', login);
 
 export default router;
