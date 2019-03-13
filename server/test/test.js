fead11c503082f0fd4f7ff7edb399f39d36896a5
Raymond Gakwaya <raymond42.gr@gmail.com>
2019-03-13 19:56:04 +0200

    [chore #164598341] add test for  failing to get any user

--- server/test/test.js
+++ server/test/test.js
@@ -9,9 +9,9 @@ chai.should();
 
 // get all users
 
-describe('list all the users', () => {
+describe('users', () => {
 
-it('should list ALL users /', (done) => {
+it('should get all users /', (done) => {
     chai.request(server)
       .get('/api/v1/users/contacts')
       .end((err, res)=>{
@@ -20,4 +20,15 @@ it('should list ALL users /', (done) => {
         done();
       });
   });
+
+
+it('should not get any user /', (done) => {
+  chai.request(server)
+    .get('/api/v1/users')
+    .end((err, res)=>{
+      res.should.have.status(404);
+      res.body.should.be.an('object');
+      done();
+    });
+});
 });
