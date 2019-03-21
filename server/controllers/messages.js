7470ee0bc6fd65be5925cc4005b626889cbec867
Raymond Gakwaya <raymond42.gr@gmail.com>
2019-03-21 17:26:02 +0200

    [feature #164802977] endpoint for user sending the message

--- server/controllers/messages.js
+++ server/controllers/messages.js
@@ -1,3 +1,5 @@
+/* eslint-disable import/prefer-default-export */
+/* eslint-disable max-len */
 /* eslint-disable no-shadow */
 /* eslint-disable radix */
 /* eslint-disable no-undef */
@@ -34,87 +36,4 @@ export const composeMessages = ((req, res) => {
         data: msgdb.rows[0],
       })))
     .catch(error => res.status(500).json({ error }));
-});
-
-// get all messages
-export const getMessages = ((req, res) => {
-  if (!messages.length) res.status(404).send('no email found');
-  res.status(200).json({
-    status: 200,
-    data: messages,
-
-  });
-});
-
-//    get one message
-export const getOneMessage = ((req, res) => {
-  const email = messages.find(email => email.id === parseInt(req.params.id));
-  if (!email) {
-    res.status(404).send('email you are looking for is not found');
-  } else {
-    res.status(200).json({
-      status: 200,
-      data: email,
-
-    });
-  }
-});
-
-//    get read messages
-export const getReadMessages = ((req, res) => {
-  const readMessages = messages.filter(email => email.status === 'read');
-  if (!readMessages.length) res.status(404).send('there is no read email');
-  res.status(200).json({
-    status: 200,
-    data: readMessages,
-  });
-});
-
-//    get unread messages
-export const getUnReadMessages = ((req, res) => {
-  const UnReadMessages = messages.filter(email => email.status === 'unread');
-  if (!UnReadMessages.length) res.status(404).send('there is no unread email');
-  res.status(200).json({
-    status: 200,
-    data: UnReadMessages,
-  });
-});
-
-// get received emails
-export const getReceivedMessages = ((req, res) => {
-  const receivedMessages = messages.filter(email => email.status === 'received');
-  if (!receivedMessages.length) res.status(404).send('there is no received email');
-  res.status(200).json({
-    status: 200,
-    data: receivedMessages,
-  });
-});
-
-//    delete message
-export const deleteMessage = ((req, res) => {
-  const deleteEmail = messages.find(email => email.id === parseInt(req.params.id));
-
-  if (!deleteEmail) {
-    res.status(400).send({ status: 400, error: 'no email to delete' });
-  } else {
-    const index = messages.indexOf(deleteEmail);
-    messages.splice(index, 1);
-    res.status(200).send({
-      status: 200,
-      data: {
-        id: req.params.id,
-        message: 'email deleted',
-      },
-    });
-  }
-});
-
-//  get sent messages
-export const getSentMessages = ((req, res) => {
-  const sentMessages = messages.filter(email => email.status === 'sent');
-  if (!sentMessages.length) res.status(404).send('there is no sent email');
-  res.status(200).json({
-    status: 200,
-    data: sentMessages,
-  });
 });
--- server/routes/messages.js
+++ server/routes/messages.js
@@ -1,24 +1,10 @@
 import express from 'express';
+
+
+import { composeMessages } from '../controllers/messages';
+
 const router = express.Router();
 
-
-import {composeMessages} from '../controllers/messages';
-import {getMessages} from '../controllers/messages';
-import {getOneMessage} from '../controllers/messages';
-import {getReadMessages} from '../controllers/messages';
-import {getUnReadMessages} from '../controllers/messages';
-import {getReceivedMessages} from '../controllers/messages';
-import {getSentMessages} from '../controllers/messages';
-import {deleteMessage} from '../controllers/messages';
-
-
 router.post('/api/v2/messages', composeMessages);
-router.get('/api/v2/messages', getMessages);
-router.get('/api/v2/messages/read', getReadMessages);
-router.get('/api/v2/messages/unread', getUnReadMessages);
-router.get('/api/v2/messages/received', getReceivedMessages);
-router.get('/api/v2/messages/sent', getSentMessages);
-router.delete('/api/v2/messages/:id', deleteMessage);
-router.get('/api/v2/messages/:id', getOneMessage);
 
 export default router;
